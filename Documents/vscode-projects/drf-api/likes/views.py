e8d3c9eef071101df81664c0d3bb43e2a9adbe30
DR-developer98 <xhfnpf57@students.codeinstitute.net>
2025-09-18 22:55:27 +0200

    Create Like list and detail generic views and hook up the corresponding urls

--- Documents/vscode-projects/drf-api/likes/views.py
+++ Documents/vscode-projects/drf-api/likes/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from DRF_API.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    List likes or create a like if logged in
+    """
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    query = Like.objects.all()
+
+    def perfom_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    query = Like.objects.all()
