1eced838ba9fd6807e65bee00c960b5cd639fa68
DR-developer98 <xhfnpf57@students.codeinstitute.net>
2025-09-18 22:19:28 +0200

    Create CommentList and CommentDetail views using generic views

---
+++ Documents/vscode-projects/drf-api/comments/views.py
@@ -0,0 +1,30 @@
+from rest_framework import generics, permissions
+from DRF_API.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
+
+# generics. (= generic view) .List(=verzorgt GET) Create(=verzorgt PUT)
+class CommentList(generics.ListCreateAPIView):
+    """
+    List comments or create a comment if logged in.
+    """
+    # Dit zorgt voor een mooi invulformulier
+    serializer_class = CommentSerializer
+    # Voorkomt dat anonieme gebruikers opmerkingen plaatsen
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    # we willen hier alle instanties van de Comments onder een post
+    queryset = Comment.objects.all()
+
+    # Dit zorgt ervoor dat comments automatisch gekoppeld worden 
+    # aan de gebruiker die ze plaats, op het moment dat
+    # de gebruiker wordt aangemaakt.
+    # Dus: gebruiker is aangemaakt => alle opmerkingen die hij zal achterlaten
+    # zullen met hem geassocieerd worden
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPI):
+    permission_classes = [IsOwnerOrReadOnly]
+    serialier_class = CommentDetailSerializer
+    queryset = Comment.objects.all()
