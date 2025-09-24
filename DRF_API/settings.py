ed2fc16a7a1918bcf97863eeea74e50107adbb18
DR-developer98 <xhfnpf57@students.codeinstitute.net>
2025-09-24 23:34:51 +0200

    Adjust settings.py

--- DRF_API/settings.py
+++ DRF_API/settings.py
@@ -40,7 +40,7 @@ REST_FRAMEWORK = {
 if 'DEV' not in os.environ:
 	REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
 		'rest_framework.renderers.JSONRenderer',
-		]
+	]
 
 # Staat token authentication toe
 REST_USE_JWT = True
@@ -144,17 +144,14 @@ WSGI_APPLICATION = 'DRF_API.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DEV' in os.environ:
-	DATABASES = {
-		'default': {
-			'ENGINE': 'django.db.backends.sqlite3',
-			'NAME': BASE_DIR / 'db.sqlite3',
-		}
-	}
-else:
-	DATABASES = {
-		'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-	}
+DATABASES = {
+	'default': ({
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': BASE_DIR / 'db.sqlite3',
+	} if 'DEV' in os.environ else dj_database_url.parse(
+		os.environ.get('DATABASE_URL')
+	))
+}
 
 
 # Password validation
